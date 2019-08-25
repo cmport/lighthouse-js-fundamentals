d2e9868b8e1dac99b70810fcee91f058f5e8e05f
cmport <portillomcarlos@hotmail.com>
2019-08-25 17:43:44 +0000

    commiting hello world

---
+++ hello.js
@@ -0,0 +1,12 @@
+/*const sayHello = function() {
+  console.log('Hello World' );
+}
+sayHello();
+*/
+
+const sayHello = function(name) {
+  console.log('Hello ' + name);
+}
+sayHello('Calinna');
+sayHello('Miranda');
+sayHello('Ferdinand');
