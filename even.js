c8503429c96aa72e1b32436d5404c94e85e9bece
cmport <portillomcarlos@hotmail.com>
2019-08-25 17:57:53 +0000

    Even and return

---
+++ even.js
@@ -0,0 +1,19 @@
+/*const isEven = function(num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elenIsEven);
+*/
+
+///simplify the code by:
+
+const isEven = function(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
